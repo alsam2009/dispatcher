ea5a2d0bbb2c327c32ee7e7775b701463bc57839
alsam2009 <den.fstack@ya.ru>
2023-10-09 15:55:03 +0300

    fix bug

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from 'next/link';
 import Image from "next/image";
 import car from "@/assets/car-login.jpg";
 import Logo from "@/app/components/Logo/Logo";
@@ -7,7 +8,9 @@ import Enter from "@/app/components/Enter/Enter";
 export default function Auth() {
   return (
     <div className="flex max-w-full h-screen">
+      <Link href="https://%D0%9E%D1%80%D0%B5%D0%BD%D0%B1%D1%83%D1%80%D0%B3-%D0%A3%D1%84%D0%B0.%D1%80%D1%84" target='_blank'>
       <Logo style={"absolute inset-0 z-10 m-2"} />
+      </Link>
       <div className="flex flex-col relative justify-center items-center w-[35%] md:hidden overflow-hidden">
         <div className="flex justify-center animate-pulse w-[20rem] border-y-2 z-10">
           <h1 className="text-4xl font-bold text-baseColor tracking-wide m-2 z-10">
